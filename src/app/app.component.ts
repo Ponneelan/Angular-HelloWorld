9424715a3919b4daa07203eb97986e56d187ac27
Ponneelan <ponneelan1305@gmail.com>
2023-05-19 12:18:06 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl:'./app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'HelloWorld';
+  flag = true
+  headtTitle = "Hi !";
+  hi = setInterval((
+    )=>{
+      this.headtTitle = this.flag ? "Login !":"Hi !";
+      this.flag = !this.flag;
+    },1000);
 }
